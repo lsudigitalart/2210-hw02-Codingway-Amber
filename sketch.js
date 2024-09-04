18712512df9d6ccd642f98dc74ba5118d8f466cb
Codingway-Amber <amberreine7@gmail.com>
2024-09-04 15:18:07 -0500

    began assignment

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(400,269);
+    background(0);
+    noStroke();
+    fill(255, 0, 0,);
+    rect(0, 0, width, 100);
+    fill(255, 0, 0, 125);
+    ellipse(100, 100, 100);
+
+    fill(221, 176, 26)
+    quad(0, 65, width, 65, width, height, 0, height);
+
+    strokeWeight(5);
+    stroke(0);
+}
+
+function draw() {
+    
+}
